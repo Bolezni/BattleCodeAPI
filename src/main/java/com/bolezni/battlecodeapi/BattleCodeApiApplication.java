becb94c88ec03146151de1ff19a59e4e7408d389
Bolezni <Yurchenkods05@gmail.com>
2025-10-05 15:19:53 +0300

    init

---
+++ src/main/java/com/bolezni/battlecodeapi/BattleCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bolezni.battlecodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleCodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleCodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolezni/battlecodeapi/BattleCodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolezni.battlecodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleCodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
